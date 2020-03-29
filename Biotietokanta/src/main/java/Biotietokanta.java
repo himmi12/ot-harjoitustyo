a116ad841099f271872161e024d25b3349e6f42b
himmi12 <62091218+himmi12@users.noreply.github.com>
2020-03-29 22:04:03 +0300

    biotietokanta

---
+++ Biotietokanta/src/main/java/Biotietokanta.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author himmi
+ */
+public class Biotietokanta {
+    
+    private Scanner lukija;
+    
+    public Biotietokanta(Scanner lukija) {
+        this.lukija=lukija;
+    }
+    
+    public void kaynnista() {
+        
+    }
+    public void lisaa() {
+        
+    }
+    public void hae() {
+        
+    }
+}
