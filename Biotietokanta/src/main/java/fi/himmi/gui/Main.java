fd6ffe24eb3977f9a1f6f2e4000734ae31274007
himmi12 <62091218+himmi12@users.noreply.github.com>
2020-04-14 13:11:23 +0300

    Oma Main-luokka jarin tekemistä varten

---
+++ Biotietokanta/src/main/java/fi/himmi/gui/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.himmi.gui;
+
+/**
+ *
+ * @author himmi
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        BiotietokantaSovellus.main(args);        
+    }
+}
